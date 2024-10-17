70d956f545919c26efb9a123ff6a43955c8935fd
Patrick Walukagga <pwalukagga@gmail.com>
2024-10-17 15:23:51 +0300

    Added region and instance_type variable types and descriptions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,11 @@
-variable "region" {}
-variable "instance_type" {}
+variable "region" {
+  type        = string
+  description = "The AWS region where resources will be created"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The EC2 instance type (e.g., t2.micro, m5.large) to use for the deployment"
+  default     = "t3.micro"
+}
